2992ef52c6a0e8bcffa22aa9293c01fdf8e9715d
Gaël PORT <gael.port@rbs.fr>
2010-08-25 11:10:47 +0200

    [FIX] Some minor fixes. [CLEAN] Some cleanings.

--- lib/Action.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @package modules.filter.lib
- */
-class filter_Action extends filter_ActionBase
-{
-}
--- lib/ActionBase.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @package modules.filter.lib
- */
-class filter_ActionBase extends f_action_BaseAction
-{
-}
