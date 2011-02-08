09a85eb0a403bbc14bf50be934028e74d7c9d1b8
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-02-08 12:41:22 +0100

    Removed addTreeAttributes/addFormAttributes on documents

--- persistentdocument/queryfolder.class.php
+++ persistentdocument/queryfolder.class.php
@@ -5,20 +5,4 @@
  */
 class filter_persistentdocument_queryfolder extends filter_persistentdocument_queryfolderbase 
 {
-	/**
-	 * @param string $moduleName
-	 * @param string $treeType
-	 * @param array<string, string> $nodeAttributes
-	 */
-//	protected function addTreeAttributes($moduleName, $treeType, &$nodeAttributes)
-//	{
-//	}
-	
-	/**
-	 * @param string $actionType
-	 * @param array $formProperties
-	 */
-//	public function addFormProperties($propertiesNames, &$formProperties)
-//	{	
-//	}
 }
