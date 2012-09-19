a25a03d75e44bfd2f73b670a4c04237d098707c0
Gaël PORT <gael.port@rbs.fr>
2012-09-19 17:40:07 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -8,16 +8,4 @@ class filter_Setup extends object_InitDataSetup
 	{
 		$this->executeModuleScript('init.xml');
 	}
-
-	/**
-	 * @return array<string>
-	 */
-	public function getRequiredPackages()
-	{
-		// Return an array of packages name if the data you are inserting in
-		// this file depend on the data of other packages.
-		// Example:
-		// return array('modules_website', 'modules_users');
-		return array();
-	}
 }
